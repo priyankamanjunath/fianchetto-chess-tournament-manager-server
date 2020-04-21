0203966697b844b3b55f89ddc601f71a5b7ff350
Jaynil Patel <jaynilbvb@gmail.com>
2020-04-21 03:15:37 -0400

    Added support for more than two players

--- index.js
+++ index.js
@@ -18,25 +18,57 @@ function handler (req, res) {
   });
 }
 
+const active_matches = require('./controller/active_matches')
+
 io.on('connection', function (socket) {
 
-  socket.on('users:get', function(data) {
-    console.log('response_users:get: ', data);
-    socket.emit('response', { type: 'users:get', data: data });
+  socket.on('users:getMove', function(data) {
+    // console.log('response_users:get: ', data);
+    socket.emit('response', { type: 'users:getMove', data: data });
   });
 
-  socket.on('message:send', function (message) {
-    message.time = new Date().getTime();
-    console.log('response_message:send: ', message);
-    socket.broadcast.emit('users:get', message);
+  socket.on('message:sendMove', function (message) {
+    message.time = new Date().getTime()
+    // console.log('response_message:send: ', message)
+    active_matches.logMatchMove(message)
+    re_add = active_matches.getRecipientAddress(message)
+    io.to(re_add).emit('users:getMove', message);
+    if(message.game_stats.over){
+      // Add game to the database using service
+
+      // remove match from here
+      active_matches.removeMatch(message)
+    }
   });
 
   socket.on('disconnect', function() {
-    console.log("Socket disconnected: ", socket);
-
+    // console.log("Socket disconnected: ", socket.id);
   });
 
+  socket.on('init_match', function (match) {
+    status = active_matches.addMatch(match, socket.id)
+    if (Object.keys(status).length == 2){
+      // {'player_color':##, last_move={}}
+      io.to(status[match.player]).emit('opponent_status', "offline");
+    }else{
+      io.to(status['b']).emit('opponent_status', "online");
+      io.to(status['w']).emit('opponent_status', "online");
+    }
+    // restore match
+     last_move = active_matches.getLastMove(match.matchId)
+    if (Object.keys(last_move).length){
+      // match started
+      io.to(socket.id).emit('restore_match', last_move)
+    }
+  })
+
+  socket.on('show_offline', function(match) {
+      status = active_matches.removePlayerFromMatch(match)
+      io.to(status[Object.keys(status)[0]]).emit('opponent_status', "offline");
+  })
   
+  // console.log("Socket Connected: ", socket.id)
+
 });
 
 console.log('server started at '+ port)
